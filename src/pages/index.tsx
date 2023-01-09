31483eca1f3a4559bf1cf8a6f9875823ad36e6bc
hamo-o <89445100+hamo-o@users.noreply.github.com>
2023-01-09 18:49:53 +0900

    Revert "Design: mypage css"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,9 @@
-import styled from '@emotion/styled';
-import COLOR from '../constants/theme';
-import { useRouter } from 'next/router';
+import GNB from "../components/global/GNB";
 
-const LandingPage = () => {
-  const router = useRouter();
-  setTimeout(() => router.push('/login'), 2000);
+export default function Home() {
   return (
-    <Container>
-      <SubTitle>방구석 셰프를 위한 1분 레시피</SubTitle>
-      <MainTitle>Recipeasy!</MainTitle>
-    </Container>
+    <>
+      <GNB />
+    </>
   );
-};
-
-const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  max-width: 450px;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background: ${COLOR.MAIN};
-`;
-
-const SubTitle = styled.div`
-  font-family: 'IBM Plex Sans KR';
-  font-weight: bold;
-  line-height: 145%;
-
-  text-align: center;
-  letter-spacing: -0.4px;
-
-  color: ${COLOR.WHITE};
-`;
-const MainTitle = styled.div`
-  font-family: 'Paytone One';
-  font-style: normal;
-  font-weight: 400;
-  font-size: 40px;
-  line-height: 150%;
-  letter-spacing: -0.022em;
-
-  color: ${COLOR.WHITE};
-`;
-
-export default LandingPage;
+}
