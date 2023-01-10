e78c6a268b6828343cedc45e9945666e80108ca1
hamo-o <yheel369@gmail.com>
2023-01-10 15:16:51 +0900

    feat: add imgcard component

--- src/components/global/GNB.tsx
+++ src/components/global/GNB.tsx
@@ -41,7 +41,7 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-around;
   border-radius: 1.5rem 1.5rem 0 0;
-  background-color: ${COLOR.GRAY1};
+  background-color: ${COLOR.BG_GRAY1};
 `;
 
 const Column = styled.div``;
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -8,6 +8,7 @@ import TopNavBar from '../../components/navigations/navigation_top';
 import { SettingIcon } from '../../components/icons/BtnIcons';
 import FONT from '../../constants/fonts';
 import COLOR from '../../constants/theme';
+import { ImgCardMedium } from '../../components/imgProps/imgcard';
 
 const MyPage = () => {
   const [user, setUser] = useState({ nickname: null, saved_recipes: [], saved_themes: [] });
@@ -28,6 +29,10 @@ const MyPage = () => {
 
   const router = useRouter();
 
+  const [nav, setNav] = useState('개별');
+
+  const handleToggleSave = (e: any) => {};
+
   return (
     <>
       <TopNavBar>
@@ -36,12 +41,59 @@ const MyPage = () => {
         </IconWrapper>
       </TopNavBar>
       <Content>
-        <Title css={FONT.HEADING}>{user.nickname}님</Title>
-        <SortNavBar>
-          <SortTitle css={FONT.SUBTITLE_2}>개별레시피</SortTitle>
-          <SortTitle css={FONT.SUBTITLE_2}>테마레시피</SortTitle>
-        </SortNavBar>
-        <TagIcon>2개의 테마레시피</TagIcon>
+        <TopInfo>
+          <Title css={FONT.HEADING}>{user.nickname}님</Title>
+          <SortNavBar>
+            <SortTitle
+              css={FONT.SUBTITLE_2}
+              onClick={() => {
+                setNav('개별');
+              }}
+              isSeleted={nav === '개별'}>
+              개별레시피
+            </SortTitle>
+            <SortTitle
+              css={FONT.SUBTITLE_2}
+              onClick={() => {
+                setNav('테마');
+              }}
+              isSeleted={nav === '테마'}>
+              테마레시피
+            </SortTitle>
+          </SortNavBar>
+        </TopInfo>
+        <CardWrapper>
+          <TagIcon css={FONT.BODY_2_2}>
+            {nav === '개별' ? user.saved_recipes.length : user.saved_themes.length}개의 {nav}레시피
+          </TagIcon>
+          {nav === '개별'
+            ? user.saved_recipes.map((recipe) => (
+                <ImgCardMedium
+                  key={1}
+                  title="레시피 이름"
+                  duration_num={0}
+                  recipe_num={0}
+                  onClick={handleToggleSave}
+                  selected={true}
+                />
+              ))
+            : user.saved_themes.map((theme) => (
+                <ImgCardMedium
+                  key={2}
+                  title="테마 이름"
+                  duration_num={0}
+                  recipe_num={0}
+                  onClick={handleToggleSave}
+                  selected={true}
+                />
+              ))}
+
+          <ImgCardMedium title="테마 이름" duration_num={0} recipe_num={0} onClick={handleToggleSave} selected={true} />
+          <ImgCardMedium title="테마 이름" duration_num={0} recipe_num={0} onClick={handleToggleSave} selected={true} />
+          <ImgCardMedium title="테마 이름" duration_num={0} recipe_num={0} onClick={handleToggleSave} selected={true} />
+          <ImgCardMedium title="테마 이름" duration_num={0} recipe_num={0} onClick={handleToggleSave} selected={true} />
+          <ImgCardMedium title="테마 이름" duration_num={0} recipe_num={0} onClick={handleToggleSave} selected={true} />
+        </CardWrapper>
       </Content>
       <GNB />
     </>
@@ -55,16 +107,23 @@ const IconWrapper = styled.div`
 
 const Content = styled.div`
   width: 100%;
-  height: 100%;
-  padding-top: 6.75rem;
+  height: auto;
+  padding: 6.75rem 0;
 
   display: flex;
   flex-direction: column;
   gap: 1.75rem;
+
+  overflow: auto;
+`;
+
+const TopInfo = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.div`
-  color: ${COLOR.BLACK};
+  color: ${COLOR.PRIMARY_BLACK};
 `;
 
 const SortNavBar = styled.div`
@@ -75,10 +134,26 @@ const SortNavBar = styled.div`
   gap: 22px;
 `;
 
-const SortTitle = styled.div``;
+const SortTitle = styled.div<{ isSeleted: boolean }>`
+  cursor: pointer;
+  color: ${(props) => (props.isSeleted ? `${COLOR.PRIMARY_BLACK}` : `${COLOR.TYPEFACE_GRAY2}`)};
+`;
 
 const TagIcon = styled.div`
-  background: ${COLOR.MAIN};
+  width: 118px;
+  height: 28px;
+  padding: 4px 12px;
+
+  color: ${COLOR.PRIMARY_ORANGE};
+  background: ${COLOR.PRIMARY_ORANGE2};
+
+  border-radius: 8px;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
 `;
 
 export default MyPage;
