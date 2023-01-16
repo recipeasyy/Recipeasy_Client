270338d7b4d3abcc70368c63178a46da64526d5a
hamo-o <yheel369@gmail.com>
2023-01-16 11:10:39 +0900

    feat: next.cofig 파일에서 직접 url 접근 막기

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/home',
+        permanent: false,
+      },
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
