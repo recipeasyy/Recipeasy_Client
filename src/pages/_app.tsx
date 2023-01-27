807da58c9ea65100898009ea0948b7257771b78c
hamo-o <yheel369@gmail.com>
2023-01-28 08:43:28 +0900

    feat: 창 끄면 로그아웃

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import '../../styles/globals.css';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+import { removeCookie } from '../util/cookie';
+import { useEffect } from 'react';
+
 const client = new QueryClient({
   defaultOptions: {
     queries: {
@@ -13,7 +16,16 @@ const client = new QueryClient({
   },
 });
 
+const logout = () => {
+  removeCookie('accessToken');
+  removeCookie('refreshToken');
+};
+
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    window.addEventListener('beforeunload', logout);
+  }, []);
+
   return (
     <QueryClientProvider client={client}>
       {process.env.NODE_ENV !== 'production' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
