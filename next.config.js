de941c94666c7612d20d1f07622e4e47c4583c0b
hamo-o <yheel369@gmail.com>
2023-01-28 09:49:44 +0900

    feat: ready pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+
 const nextConfig = {
   reactStrictMode: false,
-  async redirects() {
-    return [
-      {
-        source: '/login',
-        destination: '/home',
-        permanent: false,
-      },
-    ];
-  },
 };
+
+module.exports = withPlugins(
+  [
+    [
+      withPWA,
+      {
+        pwa: {
+          dest: 'public',
+        },
+      },
+    ],
+    // 추가 플러그인 작성
+  ],
+  nextConfig,
+);
 
 module.exports = nextConfig;
