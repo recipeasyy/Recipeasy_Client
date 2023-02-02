75ab7aa919c6ff460c27174562ddbd37e8427248
hamo-o <yheel369@gmail.com>
2023-02-02 17:29:40 +0900

    feat: 검색창 확대 방지

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/img/icon-192x192.png"></link>
         <link rel="shortcut icon" href="https://recipeasy.co.kr/favicon.ico" type="image/x-icon" />
