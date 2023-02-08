e9ec91df93c82bff8b64e75c3c1017fea5d14ced
이한비 <99820610+AlmondBreez3@users.noreply.github.com>
2023-02-08 18:11:55 +0900

    Revert "fix/pwa"

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/img/icon-192x192.png"></link>
         <link rel="shortcut icon" href="https://recipeasy.co.kr/favicon.ico" type="image/x-icon" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <body>
         <Main />
