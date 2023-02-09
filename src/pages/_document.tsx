9e088dcd6d16bc821aebf2d11d333aee986b848a
hamo-o <yheel369@gmail.com>
2023-02-09 15:53:55 +0900

    fix: add splash img

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,9 +7,19 @@ export default function Document() {
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
-        <link rel="apple-touch-startup-image" href="/img/landing.png"></link>
         <link rel="shortcut icon" href="https://recipeasy.co.kr/favicon.ico" type="image/x-icon" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/828x1792.png"
+          media="(device-width: 828px) and (device-height: 1792px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="/img/1125x2436.png"
+          media="(device-width: 1125px) and (device-height: 2436px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
       </Head>
       <body>
         <Main />
