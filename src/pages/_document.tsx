20e38cf9e0bbd92bbbbbbca696ca6e6a39922468
hamo-o <yheel369@gmail.com>
2023-02-09 15:33:48 +0900

    fix: change href

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icon-192x192.png"></link>
-        <link rel="apple-touch-startup-image" href="/landing.png"></link>
+        <link rel="apple-touch-startup-image" href="/img/landing.png"></link>
         <link rel="shortcut icon" href="https://recipeasy.co.kr/favicon.ico" type="image/x-icon" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
